695b971fb7b5a9d525dab5b323076cd56d82d049
bckyrd-io <host@bckyrd.io>
2023-05-26 06:18:38 -0700

    removed

--- platform/src/App.jsx
+++ platform/src/App.jsx
@@ -1,37 +1,32 @@
-import React from 'react';
 import './App.css';
 
 function App() {
 
   return (
     <>
-      <main>
-        <section className='dashboard'>
-          <span className="action">
-            <p>Maker Spaces</p>
-            <h4>0</h4>
-          </span>
-          <span className="action">
-            <p>Hackathons</p>
-            <h4>0</h4>
-          </span>
-          <span className="action">
-            <p>Build Publish</p>
-            <h4>0</h4>
-          </span>
-          <span className="action">
-            <p>Projects</p>
-            <h4>0</h4>
-          </span>
-        </section>
-        
-        <section className='content'>
-          <h1>Where Stuff <br /> Gets Built.</h1>
-          <p><a href="/" className='active'>bckyrd</a> platform, provides a place to work, showcase projects marketplace, hackathon challenges and progresses stuff built in unusual workspaces by enthusiast.</p>
-          <a href="https://work.bckyrd.io" className='get-started'>Start</a>
-        </section>
+      <header className='dashboard'>
+        <span className="action">
+          <p>Maker Spaces</p>
+          <h4>0</h4>
+        </span>
+        <span className="action">
+          <p>Hackathons</p>
+          <h4>0</h4>
+        </span>
+        <span className="action">
+          <p>Build Publish</p>
+          <h4>0</h4>
+        </span>
+        <span className="action">
+          <p>Projects</p>
+          <h4>0</h4>
+        </span>
+      </header>
 
-
+      <main className='content'>
+        <h1>Where Stuff <br /> Gets Built.</h1>
+        <p><a href="/" className='active'>bckyrd</a> platform, provides a place to work, showcase projects marketplace, hackathon challenges and progresses stuff built in unusual workspaces by enthusiast.</p>
+        <a href="https://work.bckyrd.io" className='get-started'>Start</a>
       </main>
     </>
   )
