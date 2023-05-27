2f301961d2510ee83ecf19482ed3f8d164e8d1ea
bckyrd-io <host@bckyrd.io>
2023-05-27 08:30:52 -0700

    trying to achieve minimalistic design

--- platform/src/App.jsx
+++ platform/src/App.jsx
@@ -10,23 +10,24 @@ function App() {
           <h4>0</h4>
         </span>
         <span className="action">
-          <p>Hackathons</p>
+          <p>Maker Spaces</p>
           <h4>0</h4>
         </span>
         <span className="action">
-          <p>Build Publish</p>
+          <p>Maker Spaces</p>
           <h4>0</h4>
         </span>
-        <span className="action">
-          <p>Projects</p>
-          <h4>0</h4>
-        </span>
+      
+      
+       
       </header>
 
       <main className='content'>
         <h1>Where Stuff <br /> Gets Built.</h1>
         <p><a href="/" className='active'>bckyrd</a> platform, provides a place to work, showcase projects marketplace, hackathon challenges and progresses stuff built in unusual workspaces by enthusiast.</p>
-        <a href="https://work.bckyrd.io" className='get-started'>Start</a>
+        <a href="https://work.bckyrd.io" className='btn'>Start</a>
+      
+       
       </main>
     </>
   )
