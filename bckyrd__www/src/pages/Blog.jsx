fb3d53ed07c8840a6458f98668643475d71f0f52
bckyrd <bckyrd.io@gmail.com>
2023-08-02 17:11:52 +1000

    new update on css

--- bckyrd__www/src/pages/Blog.jsx
+++ bckyrd__www/src/pages/Blog.jsx
@@ -1,93 +1,148 @@
 import { Link } from "react-router-dom";
-import { GoLinkExternal, GoChevronLeft, GoCommentDiscussion } from "react-icons/go";
-import bckyrd from '../assets/bckyrd.png';
-import pmodule from '../assets/pmodule.png';
-import riseapp from '../assets/riseapp.png';
-import web from '../assets/web.svg';
+import {
+  GoLinkExternal,
+  GoChevronLeft,
+  GoCommentDiscussion,
+} from "react-icons/go";
+import bckyrd from "../assets/bckyrd.png";
+import pmodule from "../assets/pmodule.png";
+import riseapp from "../assets/riseapp.png";
+import web from "../assets/web.svg";
 
 const Blog = () => {
-    return (
-        <>
-            <main className="row article" id="main">
-                <section className="col l">
-                    <img src={bckyrd} loading="lazy" alt="image web" />
-                    <p>We Are developing a web platform tool that streamlines backyard development and collaboration. Think of it as a GitHub for all types of developers, not just coders. Our platform will enable faster development and enhance collaboration
-                        among backyard innovators
-                    </p>
-                </section>
-                <section className="col r">
-                    <h1>
-                        <Link to={"github.com"}>#bckyrd</Link></h1>
-                    <p className="h2" > we are working on tools to progress creativity done at backyards
-                    </p>
-                    <div className="icon row">
-                        <Link to={"/"} className="active" ><GoChevronLeft /></Link>
-                        <Link to={"/blog"} className="" ><GoLinkExternal /></Link>
-                    </div>
-                </section>
-            </main>
+  return (
+    <>
+      <article className="row article" id="main">
+        <section className="col l">
+          <img src={bckyrd} loading="lazy" alt="image web" />
+          <p>
+            We Are developing a web platform tool that streamlines backyard
+            development and collaboration. Think of it as a GitHub for all types
+            of developers, not just coders. Our platform will enable faster
+            development and enhance collaboration among backyard innovators
+          </p>
+        </section>
+        <section className="col r">
+          <h1>
+            <Link to={"github.com"}>#bckyrd</Link>
+          </h1>
+          <p className="h2">
+            {" "}
+            we are working on tools to progress creativity done at backyards
+          </p>
+          <div className="icon row">
+            <Link to={"/"} className="active">
+              <GoChevronLeft />
+            </Link>
+            <Link to={"/blog"} className="">
+              <GoLinkExternal />
+            </Link>
+          </div>
+        </section>
+      </article>
 
-            <main className="row article">
-                <section className="col l">
-                    <img src={riseapp} loading="lazy" alt="image web" />
-                    <p>Our latest app is specifically designed for individuals who frequently travel with their workspaces and need to stay productive while on-the-go. With our app, you can easily access essential tools and resources from your everyday carry
-                        bag, allowing you to work efficiently regardless of where you are or what you need to accomplish.</p>
-                </section>
-                <section className="col r">
-                    <h1><Link to={""}>#riseApp</Link> </h1>
-                    <p className="h2">productivity app for those who travel with their workspace
-                    </p>
-                    <div className="icon row">
-                        <Link to={"/"} className="active" ><GoChevronLeft /></Link>
-                        <Link to={"/blog"} className="" ><GoLinkExternal /></Link>
-                    </div>
-                </section>
-            </main>
+      <article className="row article">
+        <section className="col l">
+          <img src={riseapp} loading="lazy" alt="image web" />
+          <p>
+            Our latest app is specifically designed for individuals who
+            frequently travel with their workspaces and need to stay productive
+            while on-the-go. With our app, you can easily access essential tools
+            and resources from your everyday carry bag, allowing you to work
+            efficiently regardless of where you are or what you need to
+            accomplish.
+          </p>
+        </section>
+        <section className="col r">
+          <h1>
+            <Link to={""}>#riseApp</Link>{" "}
+          </h1>
+          <p className="h2">
+            productivity app for those who travel with their workspace
+          </p>
+          <div className="icon row">
+            <Link to={"/"} className="active">
+              <GoChevronLeft />
+            </Link>
+            <Link to={"/blog"} className="">
+              <GoLinkExternal />
+            </Link>
+          </div>
+        </section>
+      </article>
 
-            <main className="row article">
-                <section className="col l">
-                    <img src={pmodule} loading="lazy" alt="image web" />
-                    <p>Our module uses multiple micropower station combined to reduce power
-                        failures with an Android app for efficient energy allocation while providing engineers with a simplified interface for complete control over the process. .</p>
-                </section>
-                <section className="col r">
-                    <h1><Link to={""}>#pModule</Link> </h1>
-                    <p className="h2"> makes controlling multiple micropower stations easier on android interface
-                    </p>
-                    <div className="icon row">
-                        <Link to={"/"} className="active" ><GoChevronLeft /></Link>
-                        <Link to={"/blog"} className="" ><GoLinkExternal /></Link>
-                    </div>
-                </section>
-            </main>
+      <article className="row article">
+        <section className="col l">
+          <img src={pmodule} loading="lazy" alt="image web" />
+          <p>
+            Our module uses multiple micropower station combined to reduce power
+            failures with an Android app for efficient energy allocation while
+            providing engineers with a simplified interface for complete control
+            over the process. .
+          </p>
+        </section>
+        <section className="col r">
+          <h1>
+            <Link to={""}>#pModule</Link>{" "}
+          </h1>
+          <p className="h2">
+            {" "}
+            makes controlling multiple micropower stations easier on android
+            interface
+          </p>
+          <div className="icon row">
+            <Link to={"/"} className="active">
+              <GoChevronLeft />
+            </Link>
+            <Link to={"/blog"} className="">
+              <GoLinkExternal />
+            </Link>
+          </div>
+        </section>
+      </article>
 
-            <main className="row">
-                <section className="col l">
-                    <p>you like how we do our stuff. And you wanna work together.
-                        just tell us.
-                    </p>
-                    <Link><button className="btn">ring <GoCommentDiscussion /></button></Link>
-                </section>
-            </main>
+      <article className="row">
+        <section className="col l">
+          <p>you wanna work together. just tell us.</p>
+          <Link>
+            <button className="btn">
+              ring <GoCommentDiscussion />
+            </button>
+          </Link>
+        </section>
+      </article>
 
-            <main className="row footer">
-                <section className="col l">
-                    <Link to={"#main"}>
-                        <img src={web} loading="lazy" alt="image web" />
-                    </Link>
-                </section>
-                <section className="col r">
-                    <h1><Link to={""}>YouFollow</Link></h1>
-                    <p className="h2">we were lomoso ,no we are mosolo.<Link to={"https://threads.com/bckyrd.io"} className="active" ># Threads</Link>
-                        <Link to={"https://github.com/bckyrd-io?tab=repositories"} className="active" ># Github</Link>
-                        <Link to={"https://www.linkedin.com/company/bckyrd-io/?viewAsMember=true"} className="active" ># LinkedIn</Link>
-                    </p>
-                </section>
-            </main>
-
-        </>
-    );
+      <article className="row footer">
+        <section className="col l">
+          <Link to={"#main"}>
+            <img src={web} loading="lazy" alt="image web" />
+          </Link>
+        </section>
+        <section className="col r">
+          <h1>
+            <Link to={""}>Follow</Link>
+          </h1>
+          <p className="h2">
+            we were lomoso ,no we are mosolo.
+            <Link
+              to={"https://github.com/bckyrd-io?tab=repositories"}
+              className="active"
+            >
+              # Github
+            </Link>
+            <Link
+              to={
+                "https://www.linkedin.com/company/bckyrd-io/?viewAsMember=true"
+              }
+              className="active"
+            >
+              # LinkedIn
+            </Link>
+          </p>
+        </section>
+      </article>
+    </>
+  );
 };
-
 
 export default Blog;
--- bckyrd__www/src/pages/Home.jsx
+++ bckyrd__www/src/pages/Home.jsx
@@ -1,191 +1,263 @@
-
-import { GoCode, GoCommentDiscussion, GoLinkExternal } from "react-icons/go";
 import { Link } from "react-router-dom";
-import { useState } from 'react';
-import web from '../assets/web.svg';
-import error from '../assets/error.svg';
-import code from '../assets/code.jpg';
-import router from '../assets/router_ft.svg';
-import drone from '../assets/drone_ft.svg';
-import pad from '../assets/gamer_ft.svg';
-import vr from '../assets/virtual_reality_ft.svg';
-
+import { useState } from "react";
+import { GoCode, GoCommentDiscussion, GoLinkExternal } from "react-icons/go";
+import web from "../assets/web.svg";
+import error from "../assets/error.svg";
+import code from "../assets/code.jpg";
+import router from "../assets/router_ft.svg";
+import drone from "../assets/drone_ft.svg";
+import pad from "../assets/gamer_ft.svg";
+import vr from "../assets/virtual_reality_ft.svg";
+import pmodule from "../assets/pmodule.png";
+import riseapp from "../assets/riseapp.png";
+import team from "../assets/team.png";
+import value from "../assets/value.png";
+import modular from "../assets/modular.png";
+import flexible from "../assets/flexible.png";
 
 const Home = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    const handleClick = (index) => {
-        setActiveIndex(index === activeIndex ? -1 : index);
-    };
+  const handleClick = (index) => {
+    setActiveIndex(index === activeIndex ? -1 : index);
+  };
 
+  return (
+    <>
+      <article className="row top" id="main">
+        <section className="col l">
+          <Link to={"#service"}>
+            <img src={web} loading="lazy" alt="image web" />
+          </Link>
+        </section>
+        <section className="col r ">
+          <h1 className="h1">
+            <Link to={"work"}>
+              where stuff <br /> gets build
+            </Link>
+          </h1>
+          <p className="h2">
+            building a platform for ...
+            <Link className="no-clr" to={""}>
+              #bckyrd
+            </Link>{" "}
+            <Link className="no-clr" to={""}>
+              #platform
+            </Link>
+          </p>
+          <div className="icon row">
+            <Link to={"/"} className="active">
+              <GoCommentDiscussion />
+            </Link>
+            <Link to={"/blog"} className="">
+              <GoLinkExternal />
+            </Link>
+          </div>
+        </section>
+      </article>
 
-    return (
-        <>
-            <main className="row top" id="main">
-                <section className="col l">
-                    <Link to={"#service"}>
-                        <img src={web} loading="lazy" alt="image web" />
-                    </Link>
-                </section>
-                <section className="col r ">
-                    <h1 className="h1"><Link to={"work"}>where stuff gets build</Link></h1>
-                    {/* <p2 className="h2">building a platform for Inventors. Scroll below to Explore a bigger picture of what we create. <Link className="no-clr" to={""}>#platform</Link> <Link className="no-clr" to={""}>#bckyrd</Link></p2> */}
-                    <p> <Link className="no-clr" to={""}>#platform</Link></p>
-                    <div className="row icon">
-                        <Link to={"https://calendly.com/bckyrd-io/"}><button className="btn">Hi <GoCommentDiscussion /></button></Link>
-                        <Link to={"/blog"}><button className="btn no-btn">Blog <GoLinkExternal /></button></Link>
-                    </div>
-                </section>
-            </main>
+      <article className="row">
+        <section className="col l accordion" id="work__accordion">
+          <div
+            className={`accordion-item ${activeIndex === 0 ? "active" : ""}`}
+            onClick={() => handleClick(0)}
+          >
+            <button className="accordion-button">
+              EMBEDDED MACHINE <GoCode />
+            </button>
+            <p className="accordion-content">
+              Build with arduino platforms and other microcontrollers on IoT,
+              Autonomous Robotics{" "}
+              <Link className="no-clr" to={""}>
+                #Precision
+              </Link>
+            </p>
+          </div>
+          <div
+            className={`accordion-item ${activeIndex === 1 ? "active" : ""}`}
+            onClick={() => handleClick(1)}
+          >
+            <button className="accordion-button">
+              MOBILE APP <GoCode />
+            </button>
+            <p className="accordion-content">
+              Code for android and crossplatform intergratable with web Api on
+              multiple Interfaces{" "}
+              <Link className="no-clr" to={""}>
+                #Ux
+              </Link>
+            </p>
+          </div>
+        </section>
+      </article>
 
+      <article className="row article">
+        <section className="col l">
+          <img src={pmodule} loading="lazy" alt="image web" />
+          <img src={riseapp} loading="lazy" alt="image web" />
+        </section>
+        <section className="col r ">
+          <p>
+            only scratched the surface of what we can do. Still interested to
+            understand. Arent You ?
+          </p>
+          <div className="icon row">
+            <Link to={"/blog"}>
+              <button className="btn">
+                ViewMore <GoLinkExternal />
+              </button>
+            </Link>
+          </div>
+        </section>
+      </article>
 
-            <main className="row">
-                <section className="col l accordion" id="work__accordion">
-                    <div
-                        className={`accordion-item ${activeIndex === 0 ? 'active' : ''}`}
-                        onClick={() => handleClick(0)}
-                    >
-                        <button className="accordion-button">EMBEDDED MACHINE <GoCode /></button>
-                        <p className="accordion-content">
-                            Build with arduino platforms and other microcontrollers on IoT, Autonomous Robotics <Link className="no-clr" to={""}>#Precision</Link>
-                        </p>
-                    </div>
-                    <div
-                        className={`accordion-item ${activeIndex === 1 ? 'active' : ''}`}
-                        onClick={() => handleClick(1)}
-                    >
-                        <button className="accordion-button">MOBILE APP <GoCode /></button>
-                        <p className="accordion-content">
-                            Code for android and crossplatform intergratable with web Api on multiple
-                            Interfaces  <Link className="no-clr" to={""}>#Ux</Link></p>
-                    </div>
-                </section>
-            </main>
+      <article className="row article">
+        <section className="col l">
+          <p>bigger picture of what we create</p>
+        </section>
+      </article>
 
-            <main className="row ">
-                <section className="row ">
-                    <div className="brick _1"></div>
-                    <div className="brick _2"></div>
-                    <div className="brick _3"></div>
-                </section>
-            </main>
+      <article className="row ">
+        <section className="col l ">
+          <Link to={"#main"}>
+            <img src={code} loading="lazy" alt="image web" />
+          </Link>
+        </section>
+        <section className="col r ">
+          <h1>
+            <Link to={""}>WhatsHere</Link>
+          </h1>
+          <p>catch the latest builds. Always doing something at bckyrd</p>
+        </section>
+      </article>
 
-            <main className="row">
-                <section className="col l">
-                    <p>only scratched the surface of what we can do. Still interested to understand. Arent You ?
-                    </p>
-                    <Link to={"/blog"}><button className="btn">ViewMore <GoLinkExternal /></button></Link>
-                </section>
-            </main>
+      <article className="row">
+        <section className="col l">
+          <p>you like how we do our stuff. #bckyrd</p>
+        </section>
+      </article>
 
-            <main className="row ">
-                <section className="col l ">
-                    <Link to={"#main"}>
-                        <img src={code} loading="lazy" alt="image web" />
-                    </Link>
-                </section>
-                <section className="col r ">
-                    <h1><Link to={""}>WhatsHere</Link></h1>
-                    <p>catch the latest builds. Always doing something at bckyrd</p>
-                </section>
-            </main>
+      <article className="row article ">
+        <section className="col l">
+          <img src={flexible} loading="lazy" alt="image web" title="" />
+          <img src={team} loading="lazy" alt="image web" title="" />
+          <img src={modular} loading="lazy" alt="image web" title="" />
+          <img src={value} loading="lazy" alt="image web" title="" />
+        </section>
+        <section className="col r ">
+          <p>
+            Always looking for new collaborators, so if you have a project you
+            think we would be a good fit for
+          </p>
+          <div className="icon row">
+            <Link to={"https://calendly.com/bckyrd-io/"}>
+              <button className="btn">
+                LetsWork <GoCommentDiscussion />
+              </button>
+            </Link>
+          </div>
+        </section>
+      </article>
 
-            <main className="row">
-                <section className="col l">
-                    <p>you like how we do our stuff. We would love to hear your ideas. #bckyrd
-                    </p>
-                </section>
-            </main>
+      <article className="row article">
+        <section className="col l">
+          <p>Scroll below to Explore</p>
+        </section>
+      </article>
 
-            <main className="row ">
-                <section className="row approach">
-                    <div className="brick">
-                        <p>FLEXIBLE</p>
-                        <p>break formal protocols to interact casually to better understand project needs.</p>
-                    </div>
-                    <div className="brick">
-                        <p>TEAM</p>
-                        <p>scale our freelance team based on project milestones to ensure fast completion.</p>
-                    </div>
-                    <div className="brick">
-                        <p>MODULAR</p>
-                        <p>utilize available resources and ideas to quickly iterate and improve on the prototype.</p>
-                    </div>
-                    <div className="brick">
-                        <p>VALUE</p>
-                        <p>deliver the required outcome that profit us to constantly advance the build.</p>
-                    </div>
-                </section>
-            </main>
+      <article className="row ">
+        <section className="col l ">
+          <iframe
+            src="https://www.youtube.com/embed/qJl9qFrHYm4? 
+                    controls"
+          ></iframe>
+          <div className="row review">
+            <img src={pad} loading="lazy" alt="image web" />
+            <img src={vr} loading="lazy" alt="image web" />
+            <img src={router} loading="lazy" alt="image web" />
+            <img src={drone} loading="lazy" alt="image web" />
+          </div>
+        </section>
+        <section className="col r ">
+          <h1>
+            <Link to={""}>ThatSetup</Link>
+          </h1>
+          <p>
+            break apart machines to review the inside workings. And use what we
+            figure to build our own projects.
+          </p>
+          <p>
+            {" "}
+            <Link className="no-clr" to={""}>
+              #electronics
+            </Link>{" "}
+            <Link className="no-clr" to={""}>
+              #enthusiast
+            </Link>{" "}
+          </p>
+        </section>
+      </article>
 
-            <main className="row">
-                <section className="col l">
-                    <p>Always looking for new collaborators, so if you have a project you think we would be a good fit for</p>
-                    <Link to={"https://calendly.com/bckyrd-io/"}><button className="btn">LetsWork <GoCommentDiscussion /></button></Link>
-                </section>
-            </main>
+      {/* Rest of your code */}
+      <article className="row">
+        <section className="col l accordion" id="faq__accordion">
+          <div
+            className={`accordion-item ${activeIndex === 0 ? "active" : ""}`}
+            onClick={() => handleClick(0)}
+          >
+            <button className="accordion-button">Hiring</button>
+            <p className="accordion-content">
+              The team we work with depends on job type. So Freelancers are good
+              fit to work with now.{" "}
+              <Link className="no-clr" to={""}>
+                #freelance
+              </Link>
+            </p>
+          </div>
+          <div
+            className={`accordion-item ${activeIndex === 1 ? "active" : ""}`}
+            onClick={() => handleClick(1)}
+          >
+            <button className="accordion-button">Investor</button>
+            <p className="accordion-content">
+              you like how we do our stuff and you wanna work together. hit us
+              on our social
+            </p>
+          </div>
+        </section>
+      </article>
 
-            <main className="row ">
-                <section className="col l ">
-                    <iframe src="https://www.youtube.com/embed/qJl9qFrHYm4? 
-                    controls">
-                    </iframe>
-                    <div className="row review">
-                        <img src={pad} loading="lazy" alt="image web" />
-                        <img src={vr} loading="lazy" alt="image web" />
-                        <img src={router} loading="lazy" alt="image web" />
-                        <img src={drone} loading="lazy" alt="image web" />
-                    </div>
-                </section>
-                <section className="col r ">
-                    <h1><Link to={""}>ThatSetup</Link></h1>
-                    <p>break apart machines to review the inside workings. And  use what we figure to build our own projects.</p>
-                    <p> <Link className="no-clr" to={""}>#electronics</Link> <Link className="no-clr" to={""}>#enthusiast</Link> </p>
-                </section>
-            </main>
-
-            {/* Rest of your code */}
-            <main className="row">
-                <section className="col l accordion" id="faq__accordion">
-                    <div
-                        className={`accordion-item ${activeIndex === 0 ? 'active' : ''}`}
-                        onClick={() => handleClick(0)}
-                    >
-                        <button className="accordion-button">Hiring</button>
-                        <p className="accordion-content">
-                            The team we work with depends on job type. So Freelancers are good fit to work with now. <Link className="no-clr" to={""}>#freelance</Link>
-                        </p>
-                    </div>
-                    <div
-                        className={`accordion-item ${activeIndex === 1 ? 'active' : ''}`}
-                        onClick={() => handleClick(1)}
-                    >
-                        <button className="accordion-button">Investor</button>
-                        <p className="accordion-content">
-                            you like how we do our stuff and you wanna work together. hit us on our social
-                        </p>
-                    </div>
-                </section>
-            </main>
-
-            <main className="row footer ">
-                <section className="col l ">
-                    <Link to={"#main"}>
-                        <img src={error} loading="lazy" alt="image web" />
-                    </Link>
-                </section>
-                <section className="col r ">
-                    <h1><Link to={""}>YouFollow</Link></h1>
-                    <p className="h2">we were lomoso ,no we are mosolo.<Link to={"https://threads.com/bckyrd.io"} className="active" ># Threads</Link>
-                        <Link to={"https://github.com/bckyrd-io?tab=repositories"} className="active" ># Github</Link>
-                        <Link to={"https://www.linkedin.com/company/bckyrd-io/?viewAsMember=true"} className="active" ># LinkedIn</Link>
-                    </p>
-                </section>
-            </main>
-
-        </>
-    );
+      <article className="row footer ">
+        <section className="col l ">
+          <Link to={"#main"}>
+            <img src={error} loading="lazy" alt="image web" />
+          </Link>
+        </section>
+        <section className="col r ">
+          <h1>
+            <Link to={""}>Follow</Link>
+          </h1>
+          <p className="h2">
+            we were lomoso ,no we are mosolo.
+            <Link
+              to={"https://github.com/bckyrd-io?tab=repositories"}
+              className="active"
+            >
+              # Github
+            </Link>
+            <Link
+              to={
+                "https://www.linkedin.com/company/bckyrd-io/?viewAsMember=true"
+              }
+              className="active"
+            >
+              # LinkedIn
+            </Link>
+          </p>
+        </section>
+      </article>
+    </>
+  );
 };
-
 
 export default Home;
