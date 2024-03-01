d583fb8bad9e337da205ebe84eb019ebedb6b803
bckyrd <95740688+bckyrd-io@users.noreply.github.com>
2024-02-29 23:12:28 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ the page contains routes for all pages
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./components/PageIndex";
-import PageHome from "./components/PageHome";
-import Profile from './components/PageProfile';
 
 
 function App() {
@@ -16,8 +14,6 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Index />} />
-					<Route path="/Home" element={<PageHome />} />
-					<Route path="/profile" element={<Profile />} />
 				</Routes>
 			</BrowserRouter>
 		</>
