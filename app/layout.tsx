de7b97d8f8d2e55b48a8a6c3c9a757b6f6edeeb1
bckyrd <95740688+bckyrd-io@users.noreply.github.com>
2024-03-05 02:53:46 -0800

    updated with nextjs14

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Bckyrd: Where Stuff Is Build",
+  description: "The platform leverages next industrial revolution to build electronics for electronics enthusiast",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Home() {
+    return (
+        <>
+            {/* REVEAL SECTION */}
+            <section id="__start">
+                <aside className=" maxed">
+                    <article>
+                        <h1>Leverage next industrial revolution to benefit from electronics Enthusiast</h1>
+                    </article>
+                    <article>
+                        <p className="">
+                            <Link href="/about" className="btn active">Get Started </Link>
+                            <Link href="/on" className="">🔅</Link>
+                        </p>
+                    </article>
+                </aside>
+            </section>
+
+            {/* IMAGE SECTION */}
+            <section id="__back" >
+                <aside className="maxed" style={{ backgroundImage: `url("./workshop.jpg")` }}>
+                </aside>
+                <aside className="col">
+                    <article>
+                        <p className="col">Get started , with your electronics projects you wanna see . See the posts and post for others to Benefit </p>                  </article>
+                </aside>
+            </section>
+
+            {/* POST SECTION */}
+            <section id="__post" className="row reverse">
+                <aside className="col left">
+                    <article>
+                        <Image
+                            src="/hobby.webp" // Correct path, assuming 'hobby.webp' is directly in the 'public' folder
+                            alt="Company Logo"
+                            loading="lazy"
+                            // Assuming you know the dimensions of the image; for example, 500x500
+                            width={500}
+                            height={500}
+                            // If you need the image to be responsive and maintain aspect ratio, use layout='responsive'
+                            layout='responsive'
+                        />
+                        <h4>LEVERAGE </h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores facere omnis debitis provident iusto minima quos. Consequatur, nesciunt ipsam?</p>
+                    </article>
+                   
+                </aside>
+                <aside className="col right">
+                    <article className="row">
+                        <Link href="/events" className="tag">🧑‍🚀 Discover</Link>
+                        <Link href="/challenges" className="tag">🎲 Read More</Link>
+
+                    </article>
+                </aside>
+            </section>
+
+            {/* END SECTION */}
+            <section id="__talk" className="row reverse">
+                <aside className="col right" >
+                    <article className="">
+                        <Link href="/" className="active">Download App 💎</Link>
+                    </article>
+                </aside>
+                <aside className="col">
+                    <article className="col">
+                        <h2>Step into the space where you will be able to roll out your product as an electronics enthusiast Quickly </h2>
+
+                    </article>
+                </aside>
+
+            </section>
+        </>
+    )
+}
